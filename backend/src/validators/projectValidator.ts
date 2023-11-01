73ce8345b1586927460ed6e275aff256b51580bf
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-11-01 20:20:23 +0300

    finished boilerplate fro projects service

--- backend/src/validators/projectValidator.ts
+++ backend/src/validators/projectValidator.ts
@@ -6,7 +6,18 @@ export const validateProject = Joi.object().keys({
   dueDate: Joi.date().required(),
 });
 
+export const validateUpdateProject = Joi.object().keys({
+  project_id: Joi.string().min(8).required(),
+  name: Joi.string().required(),
+  description: Joi.string().min(5).required(),
+  dueDate: Joi.date().required(),
+});
 export const assignProject = Joi.object().keys({
   project_id: Joi.string().required(),
   user_id: Joi.string().required(),
+});
+
+export const validateProjectId = Joi.object().keys({
+  id: Joi.string().min(8).required(),
+  // password: Joi.string().min(8).required(),
 });
